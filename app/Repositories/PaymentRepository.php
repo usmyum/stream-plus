d68ed7b0c1d4471b862e5cad78cfec37c9a6f308
Usman Qayyum <usman@dashingdev.com>
2024-10-18 16:47:34 +0400

    array fix for payment save

--- app/Repositories/PaymentRepository.php
+++ app/Repositories/PaymentRepository.php
@@ -7,12 +7,11 @@ use App\Models\PaymentMethod;
 class PaymentRepository
 {
     /**
-     * @param \App\Models\User $user
      * @param array $paymentDetails
      * @return mixed
      */
     public function create(array $paymentDetails)
     {
-        return PaymentMethod::create([$paymentDetails]);
+        return PaymentMethod::create($paymentDetails);
     }
 }
